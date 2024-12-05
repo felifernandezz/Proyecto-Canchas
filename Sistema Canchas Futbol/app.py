f29b11c27aaa88a0c627407f096292504da20237
felifernandezz <166411467+felifernandezz@users.noreply.github.com>
2024-12-05 10:15:53 -0300

    cambie un comentario

--- Sistema Canchas Futbol/app.py
+++ Sistema Canchas Futbol/app.py
@@ -1,7 +1,7 @@
 # app.py
 from flask import Flask
 from flask_migrate import Migrate
-from extensiones import db, jwt  # Importamos db y jwt desde extensions.py
+from extensiones import db, jwt  # Importamos db y jwt desde extensiones.py
 from rutas import main_bp
 
 app = Flask(__name__)
