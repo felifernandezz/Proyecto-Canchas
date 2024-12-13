b160691c629f73522511a8fda6b5e6f9e0fea8fe
felifernandezz <166411467+felifernandezz@users.noreply.github.com>
2024-12-13 19:51:08 -0300

    agregue def

--- Sistema Canchas Futbol/rutas/canchas_disponibles.py
+++ Sistema Canchas Futbol/rutas/canchas_disponibles.py
@@ -9,3 +9,16 @@ def obtener_tipos_canchas():
     canchas = Cancha.query.all()
     tipos_canchas = [{"tipo_cancha": cancha.tipo_cancha, "cantidad": cancha.cantidad} for cancha in canchas]
     return jsonify(tipos_canchas),200
+
+@canchas_disponibles_bp.route('/cantidad/<int:cancha_id>', methods=['GET'])
+def obtener_cantidad_de_un_tipoCancha(cancha_id):
+        # Busca la cancha por su ID
+    cancha = Cancha.query.get(cancha_id)
+    if not cancha:
+        return jsonify({"error": "No se encontró una cancha con ese ID"}), 404
+
+    # Devuelve la cantidad de canchas disponibles para ese tipo
+    return jsonify({
+        "tipo_cancha": cancha.tipo_cancha,
+        "cantidad_disponible": cancha.cantidad
+    }), 200
